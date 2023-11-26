bb521699521bce00f6afd1fb99352b324fc9a250
EricFrancischett <ericmellof@gmail.com>
2023-11-26 18:05:40 -0300

    custom colors added

--- veiIncluir.php
+++ veiIncluir.php
@@ -7,7 +7,7 @@
 	<link rel="icon" type="image/png" href="imagens/favicon1.png" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" href="css/customize.css">
+	<link rel="stylesheet" href="css/customizes.css">
 </head>
 <body onload="w3_show_nav('menuVeiculo')">
 	<!-- Inclui MENU.PHP  -->
